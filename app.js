4f8a150f61a3815754a833744496973b24a90455
Pete <peter.adam.bialecki@gmail.com>
2018-07-05 20:14:56 -0500

    Additional profile and router functionality.

--- app.js
+++ app.js
@@ -1,21 +1,15 @@
-//Problem: we need a simple way to look at a users badge count and Javascript point from a web brower
+var router = require('./router.js')
 
-//Solution: Use Node.js to perform the profile look ups and server our template via HTTP
-
-//1. Create a web server
 const http = require('http');
 
 //const hostname = '127.0.0.1';
 const port = 3000;
 
 const server = http.createServer((request, response) => {
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'text/plain');
-  response.end('Hello there friend...\n');
+    router.home(request, response);
+    router.user(request, response);
 });
 
 server.listen(port, () => {
   console.log(`Node.js server running...`);
 });
-
-//2 Handle HTTP route GET / and
