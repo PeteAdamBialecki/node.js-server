14758868ee430d30215bf79f594aba3e7a141309
Pete <peter.adam.bialecki@gmail.com>
2018-07-09 18:58:26 -0500

    GET/POST redirect.

--- router.js
+++ router.js
@@ -1,15 +1,23 @@
 var Profile = require("./profile.js");
 var renderer = require("./renderer.js");
-
+var querystring = require('querystring');
 var commonHeaders = {'Content-Type': 'text/html'}
 
 function home(request, response) {
     if(request.url === '/') {
+        if(request.method.toLowerCase() === 'get') {
         response.writeHead(200, commonHeaders);
         renderer.view('header', {}, response);
         renderer.view('search', {}, response);
         renderer.view('footer', {}, response);
         response.end();
+        } else {
+            request.on('data', function(postBody){
+                var query = querystring.parse(postBody.toString());
+                response.writeHead(303, {'Location': '/' + query.username});
+                response.end();
+            });
+        }
     }
 }
 
