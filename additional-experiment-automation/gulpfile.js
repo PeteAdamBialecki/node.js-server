98885e3b24afb53048b3fe641ad36a93fc3b579a
Pete <peter.adam.bialecki@gmail.com>
2018-07-17 19:24:02 -0500

    Renamed files and variables. Added CSS and JavaScript source maps and modules.

--- additional-experiment-automation/gulpfile.js
+++ additional-experiment-automation/gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     sass = require('gulp-sass'),
+    maps = require('gulp-sourcemaps')
     coffee = require('gulp-coffee'),
     connect = require('gulp-connect'),
     uglify = require('gulp-uglify'),
@@ -10,58 +11,62 @@ var coffeeSources = ['scripts/hello.coffee'],
     jsSources = ['scripts/*.js'],
     sassSources = ['styles/*.scss'],
     htmlSources = ['**/*.html'],
-    outputDir = 'assets';
+    outputDir = 'dist';
 
 
 gulp.task('log', function() {
-  gutil.log('== My First Task ==')
+    gutil.log('== My First Task ==')
 });
 
 gulp.task('copy', function() {
-  gulp.src('index.html')
-  .pipe(gulp.dest(outputDir))
+    gulp.src('index.html')
+    .pipe(gulp.dest(outputDir))
 });
 
 gulp.task('sass', function() {
-  gulp.src(sassSources)
-  .pipe(sass({style: 'expanded'}))
+    gulp.src(sassSources)
+    .pipe(maps.init())
+    .pipe(sass({style: 'expanded'}))
     .on('error', gutil.log)
-  .pipe(gulp.dest('assets'))
-  .pipe(connect.reload())
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest('dist'))
+    .pipe(connect.reload())
 });
 
 gulp.task('coffee', function() {
-  gulp.src(coffeeSources)
-  .pipe(coffee({bare: true})
+    gulp.src(coffeeSources)
+    .pipe(coffee({bare: true})
     .on('error', gutil.log))
-  .pipe(gulp.dest('scripts'))
+    .pipe(gulp.dest('scripts'))
 });
 
 gulp.task('js', function() {
-  gulp.src(jsSources)
-  .pipe(uglify())
-  .pipe(concat('script.js'))
-  .pipe(gulp.dest(outputDir))
-  .pipe(connect.reload())
+    gulp.src(jsSources)
+    .pipe(maps.init())
+    .pipe(uglify())
+    .pipe(concat('scripts.js'))
+    .pipe(maps.write('./'))
+    .pipe(gulp.dest(outputDir))
+    .pipe(connect.reload())
 });
 
 gulp.task('watch', function() {
-  gulp.watch(coffeeSources, ['coffee']);
-  gulp.watch(jsSources, ['js']);
-  gulp.watch(sassSources, ['sass']);
-  gulp.watch(htmlSources, ['html']);
+    gulp.watch(coffeeSources, ['coffee']);
+    gulp.watch(jsSources, ['js']);
+    gulp.watch(sassSources, ['sass']);
+    gulp.watch(htmlSources, ['html']);
 });
 
 gulp.task('connect', function() {
-  connect.server({
-    root: '.',
-    livereload: true
-  })
+    connect.server({
+        root: '.',
+        livereload: true
+    })
 });
 
 gulp.task('html', function() {
-  gulp.src(htmlSources)
-  .pipe(connect.reload())
+    gulp.src(htmlSources)
+    .pipe(connect.reload())
 });
 
 gulp.task('default', ['html', 'coffee', 'js', 'sass', 'connect', 'watch']);
