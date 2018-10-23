6e388f241e123ce26359d9bf690cc69f56e2cd38
Pete <peter.adam.bialecki@gmail.com>
2018-10-22 19:51:58 -0500

    Cleaned up router.js.

--- router.js
+++ router.js
@@ -13,34 +13,19 @@ function home(request, response) {
         } else {
             request.on('data', function(postBody){
                 var query = querystring.parse(postBody.toString());
-                response.writeHead(303, {'Location': '/' + query.username});
+                response.writeHead(303, {'Location': '/' + query.sample});
                 response.end();
             });
         }
     }
 }
 
-function user(request, response) {
-    var username = request.url.replace('/','');
-        if (username.length > 0) {
+function testRoute(request, response) {
+    var sample = request.url.replace('/','');
+        if (sample.length > 0) {
             response.writeHead(200, commonHeaders);
             renderer.view('header', {}, response);
-
-            var studentProfile = new Profile(username);
-            studentProfile.on('end', function(profileJSON){
-
-                var values = {
-                    avatarUrl: profileJSON.gravatar_url,
-                    username: profileJSON.profile_name,
-                    badges: profileJSON.badges.length,
-                    javascriptPoints: profileJSON.points.JavaScript
-                }
-                renderer.view('profile', values, response)
-                renderer.view('footer', {}, response)
-                response.end();
-            });
-
-            studentProfile.on('error', function(error){
+            sample.on('error', function(error){
                 renderer.view('error', {errorMessage: error.message}, response);
                 renderer.view('index', {}, response);
                 renderer.view('footer', {}, response);
@@ -50,4 +35,4 @@ function user(request, response) {
 }
 
 module.exports.home = home;
-module.exports.user = user;
+module.exports.testRoute = testRoute;
